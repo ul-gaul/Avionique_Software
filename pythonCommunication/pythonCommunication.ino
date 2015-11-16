81d9950f473f6d0f5d642768dbbf2f671f3954b7
unknown <Maxime Menard>
2015-11-16 17:49:16 -0500

    First file added

---
+++ pythonCommunication/pythonCommunication.ino
@@ -0,0 +1,17 @@
+void setup() 
+{
+  Serial.begin(9600); //set the baudrate
+  //Serial.print("Ready\n"); //print ready once
+}
+
+void loop() 
+{
+  float sensorValue = analogRead(A0);
+  float voltage = 5*sensorValue/1023;
+  Serial.println(voltage);
+
+  //int sensorValue = analogRead(A0);
+  //Serial.write(sensorValue);
+  //Serial.write(64);
+  delay(100);
+}
