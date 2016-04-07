f264e16b347376e7c72504f7d333dca1ed2d56c2
PharaOFred <frederick.olivier.1@ulaval.ca>
2016-04-07 17:55:12 -0400

    fini analyse.ui

--- BaseStationCode/ui/flight_data.py
+++ BaseStationCode/ui/flight_data.py
@@ -1,11 +1,18 @@
 from PyQt4 import QtGui
 from .flight_dataUI import Ui_Dialog
-from .analyse import AnalyseRocketData
+from .analysis import analysisData
 
 class flightData(QtGui.QDialog,Ui_Dialog):
     def __init__(self,parent=None):
         QtGui.QDialog.__init__(self,parent)
         self.setupUi(self)
 
+    def init_widgets(self):
+        self.analyseButton.clicked.connect(self.open_analyseData)
+
+    def open_analyseData(self):
+
+        postData = analysisData(self)
+        postData.exec_()
 
 
