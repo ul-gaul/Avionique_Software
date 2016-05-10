614d9cbb8e8871f6748126fe2414856f361ed359
PharaOFred <frederick.olivier.1@ulaval.ca>
2016-05-10 00:32:26 -0400

    The code is now able to plot inside a desired graph. The method draw_plot has been heavily changed to include a target (graph name) and the data, which will come from the thread.

--- BaseStationCode/ui/flight_data.py
+++ BaseStationCode/ui/flight_data.py
@@ -2,6 +2,7 @@ from PyQt4 import QtGui
 from .flight_dataUI import Ui_Dialog
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 import random
 
@@ -10,9 +11,42 @@ class FlightData(QtGui.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
+        self.figs = {}
+        self.canvas = {}
+        self.axs = {}
+        plot_names = ['speed', 'height', 'map', 'angle']
+        for pn in plot_names:
+            fig = Figure()
+            self.canvas[pn] = FigureCanvas(fig)
+            ax = fig.add_subplot(1, 1, 1)
+
+            self.figs[pn] = fig
+            self.axs[pn] = ax
+
+        self.speedLayout.addWidget(self.canvas['speed'])
+        self.heightLayout.addWidget(self.canvas['height'])
+        self.mapLayout.addWidget(self.canvas['map'])
+        self.angleLayout.addWidget(self.canvas['angle'])
+        data = [random.random() for i in range(10)]
+#        s = sum(data)
+#        avg = round(s/len(data), 4)
+#        print(avg)
+#        ax = self.figure.add_subplot(111)
+#        ax.hold(False)
+        self.draw_plot('speed', data)
+    def init_widgets(self):
+        self.analyseButton.clicked.connect(self.open_analysedata)
+        #self.draw_plot()
+
+    def open_analysedata(self):
+        self.done(2)  # Ferme et delete la fenêtre Dialog et retourne 2 comme valeur à results dans main_window.py
+
+    def draw_plot(self, target, data):
+        self.axs[target].plot(data, '-*')
+        self.canvas[target].draw_idle()
 
 
-        self.figure = plt.figure() # FlightData.figure = matplotlib.pyplot.figure()
+"""        self.figure = plt.figure() # FlightData.figure = matplotlib.pyplot.figure()
 #       Creates a figure widget self.name = FigureCanvas(self.figure)
         self.speedGraph = FigureCanvas(self.figure)
         self.heightGraph = FigureCanvas(self.figure)
@@ -25,28 +59,4 @@ class FlightData(QtGui.QDialog, Ui_Dialog):
         self.angleLayout.addWidget(self.angleGraph)  # insert widget "angleGraph" in angleLayout
         self.ax = self.figure.add_subplot(111)
         self.ax.hold(False)
-        self.init_widgets()
-
-    def init_widgets(self):
-        self.analyseButton.clicked.connect(self.open_analysedata)
-        self.draw_plot()
-
-    def open_analysedata(self):
-        self.done(2)  # Ferme et delete la fenêtre Dialog et retourne 2 comme valeur à results dans main_window.py
-
-    def draw_plot(self):
-        data = [random.random() for i in range(10)]
-        s = sum(data)
-        avg = round(s/len(data), 4)
-        print(avg)
-#        ax = self.figure.add_subplot(111)
-#        ax.hold(False)
-        self.ax.plot(data, '-*')
-"""
-        data1 = [random.random() for i in range(10)]
-        c = sum(data1)
-        print(c)
-        ax1 = self.figure.add_subplot(122)
-        ax1.hold(False)
-        ax1.plot(data1, '*-')
-"""
+        self.init_widgets()"""
