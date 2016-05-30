12c69d4e9702bd96364a68d30205926ab3ad9e53
PharaOFred <frederick.olivier.1@ulaval.ca>
2016-05-29 21:50:09 -0400

    Added function that generates 4 random list

--- BaseStationCode/ui/flight_data.py
+++ BaseStationCode/ui/flight_data.py
@@ -3,7 +3,9 @@ from .flight_dataUI import Ui_Dialog
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import random
-
+import time
+import matplotlib.animation as Animation
+import numpy as np
 
 class FlightData(QtGui.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
@@ -25,16 +27,64 @@ class FlightData(QtGui.QDialog, Ui_Dialog):
         self.heightLayout.addWidget(self.canvas['height'])
         self.mapLayout.addWidget(self.canvas['map'])
         self.angleLayout.addWidget(self.canvas['angle'])
-
+        self.init_widgets()
+        #self.ani = Animation.FuncAnimation(self.figs["speed"], self.generate_random_list, 1000)
+        for pn in ['speed', 'height', 'map', 'angle']:
+            self.ani = Animation.FuncAnimation(self.figs[pn], self.generate_random_list, 1000)
     def init_widgets(self):
         self.analyseButton.clicked.connect(self.open_analysedata)
 
 
     def open_analysedata(self):
-        self.done(2)  # Closes and deletes Dialog Window and return the interger 2 to main_window.py which will
+        self.done(2)  # Closes and deletes Dialog Window and return the integer 2 to main_window.py which will
         #               connect to and open analysis.py
 
     def draw_plot(self, target, data):
         self.axs[target].plot(data, '-*')
         self.canvas[target].draw_idle()
+
+    def generate_random_list(self, i):
+        data_list = []
+        data_list1 = []
+        data_list2 = []
+        data_list3 = []
+        self.axs["height"].clear()
+        self.axs["speed"].clear()
+        self.axs["map"].clear()
+        self.axs["angle"].clear()
+
+        for j in range(i):
+            data_list.append(random.randrange(0,100))
+
+        for j in data_list:
+            data_list1.append(j+random.randrange(-10,10))
+            data_list2.append(j*random.randrange(0,5))
+            data_list3.append(j/random.randrange(1,10))
+        self.draw_plot("speed", data_list)
+        self.draw_plot("height", data_list1)
+        self.draw_plot("map", data_list2)
+        self.draw_plot("angle", data_list3)
+
+
+    def generate_cosinus(self, i):
+        cos = np.cos
+        yar = []
+        yar1 = []
+        yar2 = []
+        yar3 = []
+        for pn in ['speed', 'height', 'map', 'angle']:
+            self.axs[pn].clear()
+
+        for j in range(i):
+            yar.append((cos((i*500/1000)*np.pi)))
+
+        for obj in yar:
+            yar1.append(obj/4)
+            yar2.append(obj*20)
+            yar3.append(obj/10)
+
+        self.draw_plot("height", yar)
+        self.draw_plot("speed", yar1)
+        self.draw_plot("map", yar2)
+        self.draw_plot("angle", yar3)
 
--- BaseStationCode/ui/main_window.py
+++ BaseStationCode/ui/main_window.py
@@ -2,6 +2,7 @@ from .main_windowUI import Ui_MainWindow
 from PyQt4 import QtGui
 from .flight_data import FlightData
 from .analysis import analysisData
+import matplotlib.animation as Animation
 
 class MainWindow(QtGui.QMainWindow,Ui_MainWindow):
     def __init__(self, parent=None):
@@ -15,6 +16,7 @@ class MainWindow(QtGui.QMainWindow,Ui_MainWindow):
 
     def open_flightData(self):
         flight_data = FlightData(self)
+        #ani = Animation.FuncAnimation(flight_data.figs["speed"], flight_data.generate_random_list, 500)
         result = flight_data.exec_()
         if result == 2: #Si result = 2, ouvre le dialog d'analyse
             self.open_analyseDialog()
