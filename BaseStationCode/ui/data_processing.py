609a9ad87a580b40433c06a076ffaf70590ad372
PharaOFred <frederick.olivier.1@ulaval.ca>
2016-06-05 18:20:51 -0400

    Added LCD function

---
+++ BaseStationCode/ui/data_processing.py
@@ -0,0 +1,18 @@
+import numpy as np
+from math import sqrt
+
+
+class DataProcessing():
+
+    def __init__(self):
+        self.data = {"sp":[], "h":[], "magn" : [], "geo":[], "acc": [], "temp":[]}
+
+
+    def speed_processing(self):
+
+
+
+
+
+
+
--- BaseStationCode/ui/flight_data.py
+++ BaseStationCode/ui/flight_data.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from .flight_dataUI import Ui_Dialog
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -7,40 +7,49 @@ import time
 import matplotlib.animation as Animation
 import numpy as np
 
+
 class FlightData(QtGui.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
-        self.figs = {}
-        self.canvas = {}
-        self.axs = {}
+        self.figs = {}  # Empty Dictionnary
+        self.canvas = {}  # Empty Dictionnary
+        self.axs = {}  # Empty Dictionnary
         plot_names = ['speed', 'height', 'map', 'angle']
         for pn in plot_names:
-            fig = Figure()
-            self.canvas[pn] = FigureCanvas(fig)
-            ax = fig.add_subplot(1, 1, 1)
+            fig = Figure()  # Give the variable fig the function Figure()
+            self.canvas[pn] = FigureCanvas(fig)  # Creates the key pn and assiociates it with FigureCanvas
+            ax = fig.add_subplot(1, 1, 1)  # Creates a plot
 
-            self.figs[pn] = fig
-            self.axs[pn] = ax
+            self.figs[pn] = fig # Each string of plot_names are a Figure()
+            self.axs[pn] = ax  # Each string of plot_names are a plot defined by ax
 
-        self.speedLayout.addWidget(self.canvas['speed'])
-        self.heightLayout.addWidget(self.canvas['height'])
-        self.mapLayout.addWidget(self.canvas['map'])
-        self.angleLayout.addWidget(self.canvas['angle'])
-        self.init_widgets()
-        #self.ani = Animation.FuncAnimation(self.figs["speed"], self.generate_random_list, 1000)
-        for pn in ['speed', 'height', 'map', 'angle']:
-            self.ani = Animation.FuncAnimation(self.figs[pn], self.generate_random_list, 1000)
+        self.speedLayout.addWidget(self.canvas['speed'])  # Creates the canvas for speed
+        self.heightLayout.addWidget(self.canvas['height'])  # Creates the canvas for height
+        self.mapLayout.addWidget(self.canvas['map'])  # Creates the canvas for map
+        self.angleLayout.addWidget(self.canvas['angle'])  # Creates the canvas for angle
+        self.init_widgets()  # Once the Dialog exist, automatically initiate self.init_widgets
+
     def init_widgets(self):
         self.analyseButton.clicked.connect(self.open_analysedata)
+        #self.stopButton.clicked.connect(self.stop_plotting)
+        self.startButton.clicked.connect(self.start_plotting)
+        self.showlcd([10,2,14])
 
 
     def open_analysedata(self):
         self.done(2)  # Closes and deletes Dialog Window and return the integer 2 to main_window.py which will
         #               connect to and open analysis.py
 
+    #def stop_plotting(self):
+
+
+    def start_plotting(self):
+        for pn in ['speed', 'height', 'map', 'angle']:
+            self.ani = Animation.FuncAnimation(self.figs[pn], self.generate_random_list, 1000)
+
     def draw_plot(self, target, data):
-        self.axs[target].plot(data, '-*')
+        self.axs[target].plot(data, '-*')  # Will plot with one of the 4 plot_names and any given data in a list
         self.canvas[target].draw_idle()
 
     def generate_random_list(self, i):
@@ -85,6 +94,14 @@ class FlightData(QtGui.QDialog, Ui_Dialog):
 
         self.draw_plot("height", yar)
         self.draw_plot("speed", yar1)
-        self.draw_plot("map", yar2)
+        self.draw_plot("map", self.yar2)
         self.draw_plot("angle", yar3)
+
+    def showlcd(self, data):
+        palette = self.speedLCD.palette()
+        palette.setColor(palette.WindowText, QtGui.QColor(85, 85, 255))
+        speed = data[len(data)-1]
+        text = str(speed)
+        self.speedLCD.display(text)
+
 
