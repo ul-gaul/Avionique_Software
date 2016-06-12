a20f3652cf42e7f581a9ae853e708aa02741de39
MaitreMenard <maxime.menard.1@ulaval.ca>
2016-06-12 15:16:38 -0400

    Clean up du repo

--- Interface/flight_data.py
+++
@@ -1,8 +0,0 @@
-from PyQt4 import QtGui
-from flight_dataUI import Ui_Dialog
-
-class flightData(QtGui.QDialog,Ui_Dialog):
-    def __init__(self,parent=None):
-        QtGui.QDialog.__init__(self,parent)
-        self.setupUi(self)
-
