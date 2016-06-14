622d17ecc43cb279976a273ee9c20689d0e0d44d
MaitreMenard <maxime.menard.1@ulaval.ca>
2016-06-13 20:39:34 -0400

    Ajout du traitement des données

--- BaseStationCode/ui/data_processing.py
+++ BaseStationCode/ui/data_processing.py
@@ -23,15 +23,17 @@ class DataProcessing:
         self.data["long2"] = []
         self.data["temp1"] = []
         self.data["temp2"] = []
+        self.data["verticalSpeed"] = [0]
+        self.data["meanlat"] = []
+        self.data["meanlong"] = []
 
         self.acc = []
-        self.speed = [0]
         self.acheckpoint = 0
 
     def add_data(self, dataList):
         for j in range(len(dataList)):
             self.data["time"].append(dataList[j].timeStamp)
-            self.data["accx"].append(dataList[j].acceleration_x)
+            self.data["accx"].append(-dataList[j].acceleration_x)
             self.data["accy"].append(dataList[j].acceleration_y)
             self.data["accz"].append(dataList[j].acceleration_z)
             self.data["angx"].append(dataList[j].angular_speed_x)
@@ -48,14 +50,15 @@ class DataProcessing:
             self.data["temp1"].append(dataList[j].temperature_1)
             self.data["temp2"].append(dataList[j].temperature_2)
 
-    def acc_normalisation(self):
+            self.data["meanlat"].append((dataList[j].latitude_1 + dataList[j].latitude_2)/2)
+            self.data["meanlong"].append((dataList[j].longitude_1 + dataList[j].longitude_2)/2)
 
+        if len(self.data["alt"]) > 1:
+            self.data["verticalSpeed"].append((self.data["alt"][-1] - self.data["alt"][-2])/(self.data["time"][-1] - self.data["time"][-2]))
+
+    def acc_normalisation(self):
         if len(self.data["accx"]) == len(self.data["accy"]) and len(self.data["accx"]) == len(self.data["accz"]):
 
             for j in range(len(self.data["accx"])):
                 self.acc.append(sqrt((self.data["accx"][j+self.acheckpoint]**2)+(self.data["accy"][j+self.acheckpoint]**2)+(self.data["accz"][j+self.acheckpoint]**2)))
             self.acheckpoint = len(self.data["accx"])
-
-    def speed_norm(self):
-        for j in range(len(self.acc)):
-            self.speed.append(self.speed[j]+self.acc[j]*self.data["time"][j])
