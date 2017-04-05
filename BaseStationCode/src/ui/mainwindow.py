f64831216a96e963b30f869c43f594d9b507395a
PharaOFred <frederick.olivier.1@ulaval.ca>
2017-04-04 20:29:11 -0400

    Adddin openFileName and saveFileName method

--- BaseStationCode/src/ui/mainwindow.py
+++ BaseStationCode/src/ui/mainwindow.py
@@ -1,5 +1,6 @@
 from .mainwindowUI import Ui_MainWindow
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5 import QtCore, QtGui
 from datetime import datetime as d
 
 
@@ -10,15 +11,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.initUI()
 
     def initUI(self):
-        pass
+        self.pushButton.clicked.connec
 
     def openFileName(self):
 
-        self.filename, _= QFileDialog.getOpenFileName()
+        self.filename, _= QFileDialog.getOpenFileName(self, "Open File")
 
     def saveFileName(self):
 
-        self.filename, _ = QFileDialog.getSaveFileName(self, "Save File", d.now().strftime())
+        self.filename, _ = QFileDialog.getSaveFileName(self, "Save File", d.now().strftime("%Y-%m-%d_%Hh%Mm")+".csv",
+                                                       "All Files (*);; CSV Files (*.csv)")
 
 
 
