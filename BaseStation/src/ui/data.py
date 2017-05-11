cac32b180022a0e53569ec93eb77291c4818220f
MaitreMenard <maxime.menard.1@ulaval.ca>
2017-05-10 23:17:32 -0400

    Création de la classe ExtendedQSlider

--- BaseStation/src/ui/data.py
+++ BaseStation/src/ui/data.py
@@ -1,15 +1,16 @@
 from datetime import datetime as d
-from PyQt5.QtWidgets import QWidget, QFileDialog, QApplication
+from PyQt5.QtWidgets import QFileDialog, QSlider
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import QIcon
 
-from .dataUI import Ui_Data
+from src.ui.dataUI import Ui_Data
+from src.ui.ExtendedQSlider import ExtendedQSlider
 
 
 class Data(Ui_Data):
 
     def __init__(self, main_window):
         self.main_window = main_window
+        self.horizontalSlider = None
         self.filename = ""
         self.setupUi(self.main_window)
         self.initUI()
@@ -18,9 +19,22 @@ class Data(Ui_Data):
         f = open("resources/data.css", 'r')
         stylesheet = f.read()
         self.main_window.setStyleSheet(stylesheet)
+        self.setup_slider()
         self.label.setPixmap(QtGui.QPixmap("resources/logo.jpg"))
-        QApplication.setWindowIcon(QtGui.QIcon("resource/logo.jpg"))
 
+        self.main_window.setWindowIcon(QtGui.QIcon("resource/logo.jpg"))
+
+    def setup_slider(self):
+        self.horizontalSlider = ExtendedQSlider(self.centralwidget)
+        self.horizontalSlider.setEnabled(True)
+        self.horizontalSlider.setMaximum(100)
+        self.horizontalSlider.setPageStep(1)
+        self.horizontalSlider.setSliderPosition(0)
+        self.horizontalSlider.setTracking(True)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setTickPosition(QSlider.NoTicks)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.verticalLayout_5.addWidget(self.horizontalSlider)
 
     def openFileName(self):
 
