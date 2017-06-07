c3402315f4025d348aadb6ca0b111849c939ab0f
MaitreMenard <maxime.menard.1@ulaval.ca>
2017-06-07 00:24:45 -0400

    Ajout de unit tests pour la classe Consumer

---
+++ BaseStation/tests/consumer_test.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import Mock
+from src.consumer import Consumer
+from src.producer import Producer
+from src.rocket_packet import RocketPacket
+
+
+class ConsumerTest(unittest.TestCase):  # TODO: make a main module for running unittest.main()
+
+    def setUp(self):
+        self.producer = Producer()
+        self.consumer = Consumer(self.producer)
+
+    def test_constructor(self):
+        self.assertEqual(self.consumer.producer, self.producer, "The Consumer's producer property wasn't set correctly")
+        self.assertFalse(self.consumer.has_new_data, "The Consumer's has_new_data property should be False")
+        self.assertEqual(RocketPacket().__dict__.keys(), self.consumer.data.keys())
+
+    def test_getitem(self):
+        time_stamps = [0, 1, 2, 3, 4, 5]
+
+        self.consumer.data["time_stamp"] = time_stamps
+
+        self.assertEqual(self.consumer["time_stamp"], time_stamps)
+        self.assertEqual(self.consumer["altitude"], [])
+        self.assertRaises(KeyError, self.consumer.__getitem__, "invalid_key")
+
+    def test_update(self):
+        number_of_properties = len(RocketPacket().__dict__.keys())
+        dummy_data_list1 = [i for i in range(number_of_properties)]
+        dummy_data_list2 = [2 * i for i in range(number_of_properties)]
+        rocket_packet_list = [RocketPacket(dummy_data_list1), RocketPacket(dummy_data_list2)]
+        self.producer.get_data = Mock(return_value=rocket_packet_list)
+
+        self.consumer.update()
+
+        self.assertTrue(self.consumer.has_new_data)
+        self.assertEqual(len(self.consumer["time_stamp"]), 2)
+        for i in range(len(rocket_packet_list)):
+            for key, value in rocket_packet_list[i].__dict__.items():
+                self.assertEqual(self.consumer[key][i], value)
+
+    def test_update_with_no_data(self):
+        self.producer.get_data = Mock(return_value=[])
+
+        self.consumer.update()
+
+        self.assertFalse(self.consumer.has_new_data)
+        self.assertEqual(len(self.consumer["time_stamp"]), 0)
