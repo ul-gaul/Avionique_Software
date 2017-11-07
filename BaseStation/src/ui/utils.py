9448ac678b05dd399484a41043da375aa42872e0
MaitreMenard <maxime.menard.1@ulaval.ca>
2017-11-06 21:38:46 -0500

    Retrait de la duplication dans les fonctions set_size_policy

--- BaseStation/src/ui/utils.py
+++ BaseStation/src/ui/utils.py
@@ -2,19 +2,11 @@ from PyQt5.QtWidgets import QWidget, QSizePolicy
 
 
 def set_minimum_expanding_size_policy(widget: QWidget):
-    size_policy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
-    size_policy.setHorizontalStretch(0)
-    size_policy.setVerticalStretch(0)
-    size_policy.setHeightForWidth(widget.sizePolicy().hasHeightForWidth())
-    widget.setSizePolicy(size_policy)
+    set_size_policy(widget, QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
 
 
 def set_fixed_size_policy(widget: QWidget):
-    size_policy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-    size_policy.setHorizontalStretch(0)
-    size_policy.setVerticalStretch(0)
-    size_policy.setHeightForWidth(widget.sizePolicy().hasHeightForWidth())
-    widget.setSizePolicy(size_policy)
+    set_size_policy(widget, QSizePolicy.Fixed, QSizePolicy.Fixed)
 
 
 def set_size_policy(widget: QWidget, horizontal_policy: QSizePolicy, vertical_policy: QSizePolicy):
