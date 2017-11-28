4c6beeba6cf7114adcc5a87cf94236f25a71b17c
MaitreMenard <maxime.menard.1@ulaval.ca>
2017-11-28 00:59:18 -0500

    Clean up du démarrage de l'application

---
+++ BaseStation/src/application.py
@@ -0,0 +1,35 @@
+import sys
+import ctypes
+from PyQt5 import QtWidgets
+from src.ui.mainwindow import MainWindow
+
+
+class Application:
+
+    def __init__(self):
+        self.appUserModelId = "ca.ulaval.gaul.basestation"  # must be a unicode string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(self.appUserModelId)  # todo: if windows
+
+        Application.override_exception_hook()
+
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.window = MainWindow()
+        self.window.show()
+        sys.exit(self.app.exec_())
+
+    @staticmethod
+    def override_exception_hook():
+        """
+        Back up the reference to the exception hook and set the exception hook to our wrapping function.
+        This is required to prevent Qt from crashing without printing the stacktrace.
+        """
+        initial_exception_hook = sys.excepthook
+
+        def exception_hook_wrapper(exception_type, value, traceback):
+            # Print the error and traceback
+            print(exception_type, value, traceback)
+            # Call the normal Exception hook after
+            initial_exception_hook(exception_type, value, traceback)
+            sys.exit(1)
+
+        sys.excepthook = exception_hook_wrapper
