cd5e7669a5223286f76c72a292de0e8fbf7ad7d9
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-03-12 10:35:45 -0400

    Fix du comportement du statusBar

--- BaseStation/src/ui/status_bar.py
+++ BaseStation/src/ui/status_bar.py
@@ -9,15 +9,19 @@ class StatusBar(QStatusBar, MessageListener):
     def __init__(self, parent):
         super().__init__(parent)
         self.setObjectName("status_bar")
+
         self.label = QLabel(self)
         self.label.setObjectName("status_bar_label")
         label_stylesheet = read_stylesheet("src/resources/status_bar_label.css")
         self.label.setStyleSheet(label_stylesheet)
+        self.addWidget(self.label)
 
     def notify(self, message: str, message_type: MessageType):
-        self.removeWidget(self.label)
+        self.clearMessage()
+        self.label.hide()
+
         if message_type == MessageType.INFO:
             self.showMessage(message, 3000)
         else:
             self.label.setText(message)
-            self.addWidget(self.label)
+            self.label.show()
