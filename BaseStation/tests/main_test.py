a715bc9fe5bbe2298fc1bf782502a77ca96fdbd3
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-03-25 19:34:34 -0400

    Lancement des tests par découverte

--- BaseStation/tests/main_test.py
+++ BaseStation/tests/main_test.py
@@ -1,9 +1,6 @@
 import unittest
-from tests.persistence.test_csv_data_persister import CsvDataPersisterTest
-from tests.test_consumer import ConsumerTest
-from tests.test_controller import ControllerTest
-from tests.test_serial_data_producer import SerialDataProducerTest
-from tests.test_file_data_producer import FileDataProducerTest
 
 if __name__ == "__main__":
-    unittest.main()  # TODO: use test discovery
+    tests = unittest.defaultTestLoader.discover(".")
+    test_runner = unittest.TextTestRunner()
+    test_runner.run(tests)
