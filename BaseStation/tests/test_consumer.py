326a7bcea86c31803e80d836a7ddaee041c6598e
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-10-05 17:24:20 -0400

    Testing CircleCI with failing unit tests

--- BaseStation/tests/test_consumer.py
+++ BaseStation/tests/test_consumer.py
@@ -74,3 +74,6 @@ class ConsumerTest(unittest.TestCase):
         consumer_has_data = self.consumer.has_data()
 
         self.assertFalse(consumer_has_data)
+
+    def test_fail(self):
+        self.fail("Testing CircleCI")
