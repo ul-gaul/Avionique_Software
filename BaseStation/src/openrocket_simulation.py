f23f4d03951cd46e95e349a1aa623a8364279e6b
Babouche <clnabergel@hotmail.fr>
2018-11-07 09:40:57 -0500

    Fix des bug sur la ExtendedQSlider
    Reimplantation de la methode 'mouseMoveEvent'.
    
    Reecriture de la methode 'mousePressEvent'.
    
    Creation d'une methode 'compute_position' qui evite la duplication de code ains de calculer la position de la barre en fonction du curseur.

--- BaseStation/src/openrocket_simulation.py
+++ BaseStation/src/openrocket_simulation.py
@@ -15,4 +15,5 @@ class OpenRocketSimulation:
                 self.time.append(float(data[0]))
                 self.altitude.append(float(data[1]))
         except ValueError:
+
             raise DomainError("Le fichier de simulation " + filename + " n'a pas pu être chargé")
