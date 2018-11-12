f21e2fe42d848d1f5dec6920c03361ed51097802
Babouche <clnabergel@hotmail.fr>
2018-11-12 11:25:34 -0500

    Update BaseStation/src/ui/altitude_graph.py
    Override de la methode 'draw' afin de pouvoir ecrire  sur un TextItem.
    
    Ajout de l'affichage de l'apogee.
    Fix du textItem de l'altitude.

--- BaseStation/src/ui/altitude_graph.py
+++ BaseStation/src/ui/altitude_graph.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtCore
-from pyqtgraph import PlotWidget, mkPen, mkBrush, TextItem
+from pyqtgraph import PlotWidget, mkPen, mkBrush, TextItem, LabelItem
 
 from src.ui.utils import set_minimum_expanding_size_policy
 
@@ -19,30 +19,49 @@ class AltitudeGraph(PlotWidget):
 
         self.altitude_curve = self.plot([0], [0], pen=mkPen(color='k', width=3))
 
-        self.current_altitude_text = None
+        self.current_altitude = 0
+        self.current_altitude_text = TextItem("", color='k')
         self.current_altitude_point = self.plotItem.scatterPlot([], [], pxMode=True, size=8, brush=mkBrush(color='r'))
+        self.addItem(self.current_altitude_text)
 
         self.simulation_curve = None
 
-    def draw(self, values: list):
+        self.apogee = None
+        self.apogee_text = TextItem("", anchor=(0.5, 1), color='b')
+        self.apogee_point = self.plotItem.scatterPlot([], [], pxMode=True, size=8, brush=mkBrush(color='b'))
+        self.addItem(self.apogee_text)
+
+    def paintEvent(self, ev):
+        if self.current_altitude > 0:
+            self.apogee_text.setText("{}m".format(self.apogee))
+            self.current_altitude_text.setText("{}m".format(self.current_altitude))
+
+        super(AltitudeGraph, self).paintEvent(ev)
+
+    def draw(self, values: list): # Faire attention quand on reverse, il faut changer l apogee
         nb_points = len(values)
 
+        if self.apogee is None:
+            self.apogee = max(values)
+            altitude_index = values.index(self.apogee)
+            self.apogee_point.setData([altitude_index], [self.apogee])
+            self.apogee_text.setPos(altitude_index, self.apogee)
+        elif nb_points == 1:
+            self.apogee = values[0]
+            self.apogee_point.setData([], [])
+            self.apogee_text.setPos(0, self.apogee)
+
         if nb_points > 0:
-            current_altitude = int(values[-1])
+            self.current_altitude = int(values[-1])
 
             self.altitude_curve.setData(values)
-            self.current_altitude_point.setData([nb_points - 1], [current_altitude])
-            # FIXME: this makes the app crash randomly when resizing the axis of the graph
-            # self.update_current_altitude_text(nb_points, current_altitude)
+            self.current_altitude_point.setData([nb_points - 1], [self.current_altitude])
+            self.current_altitude_text.setPos(nb_points - 1, self.current_altitude)
 
-    def update_current_altitude_text(self, nb_points: int, current_altitude: int):
-        if self.current_altitude_text is not None:
-            self.removeItem(self.current_altitude_text)
-
-        if nb_points >= 2:
-            self.current_altitude_text = TextItem(text=str(current_altitude), color='r', anchor=(1, 1))
-            self.current_altitude_text.setPos(nb_points - 1, current_altitude)
-            self.addItem(self.current_altitude_text)
+            if self.current_altitude > self.apogee:
+                self.apogee = self.current_altitude
+                self.apogee_point.setData([nb_points - 1], [self.current_altitude])
+                self.apogee_text.setPos(nb_points - 1, self.current_altitude)
 
     def set_target_altitude(self, altitude):
         self.plotItem.addLine(y=altitude, pen=mkPen(color=(15, 236, 20), width=3, style=QtCore.Qt.DashDotLine))
