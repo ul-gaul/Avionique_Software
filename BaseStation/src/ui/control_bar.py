3d6b65dbaf284f4d27ed9023b9dad1ddf1aa25b4
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-11-12 09:49:37 -0500

    Extracted private method in ControlBar.py

--- BaseStation/src/ui/control_bar.py
+++ BaseStation/src/ui/control_bar.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QMouseEvent
 from PyQt5.QtWidgets import QSlider
 from PyQt5.QtCore import Qt
 from typing import Callable
@@ -17,7 +18,10 @@ class ControlBar(QSlider):
         self.setObjectName(object_name)
         self.callback = lambda index: None
 
-    def compute_position(self, q_mouse_event):
+    def set_callback(self, new_callback: Callable[[int], None]):
+        self.callback = new_callback
+
+    def compute_position(self, q_mouse_event: QMouseEvent):
         value_range = self.maximum() - self.minimum()
 
         if self.orientation() == Qt.Vertical:
@@ -30,18 +34,15 @@ class ControlBar(QSlider):
         else:
             self.setValue(new_value)
 
-    def mousePressEvent(self, q_mouse_event):
+    def mousePressEvent(self, q_mouse_event: QMouseEvent):
         if q_mouse_event.button() == Qt.LeftButton:
-            self.compute_position(q_mouse_event)
-            self.callback(self.value())
-            q_mouse_event.accept()
+            self._handle_mouse_event(q_mouse_event)
 
-    def mouseMoveEvent(self, q_mouse_event):
+    def mouseMoveEvent(self, q_mouse_event: QMouseEvent):
+        self._handle_mouse_event(q_mouse_event)
+        super().mouseMoveEvent(q_mouse_event)
+
+    def _handle_mouse_event(self, q_mouse_event: QMouseEvent):
         self.compute_position(q_mouse_event)
         self.callback(self.value())
         q_mouse_event.accept()
-
-        super().mouseMoveEvent(q_mouse_event)
-
-    def set_callback(self, new_callback: Callable[[int], None]):
-        self.callback = new_callback
