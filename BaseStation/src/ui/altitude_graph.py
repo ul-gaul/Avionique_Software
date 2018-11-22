4e25af59a6946661c6563fbb327d57630a9d1ace
Babouche <clnabergel@hotmail.fr>
2018-11-22 11:57:42 -0500

    Update altitude_graph.py
    Une autre methode

--- BaseStation/src/ui/altitude_graph.py
+++ BaseStation/src/ui/altitude_graph.py
@@ -31,16 +31,18 @@ class AltitudeGraph(PlotWidget):
         self.apogee_point = self.plotItem.scatterPlot([], [], pxMode=True, size=8, brush=mkBrush(color='b'))
         self.addItem(self.apogee_text)
 
+        self.apogee_dict = []
+
     def paintEvent(self, ev):
         self.apogee_text.setText("{}ft".format(self.apogee))
         self.current_altitude_text.setText("{}ft".format(self.current_altitude))
 
         super(AltitudeGraph, self).paintEvent(ev)
 
-    def setApogee(self, values: list):
+    def set_apogee(self, values: list):
         min_altitude = 0
         index = 0
-        print(len(values))
+
         for i in range(len(values) - 1):
             if min_altitude < values[i]:
                 min_altitude = values[i]
@@ -50,18 +52,20 @@ class AltitudeGraph(PlotWidget):
         self.apogee_point.setData([index], [self.apogee])
         self.apogee_text.setPos(index, self.apogee)
 
-
-
     def draw(self, values: list): # Faire attention quand on reverse, il faut changer l apogee
         nb_points = len(values)
 
         if nb_points > 0:
             if self.apogee is None:
                 self.apogee = max(values)
+                self.apogee_dict.append(self.apogee)
                 altitude_index = values.index(self.apogee)
                 self.apogee_point.setData([altitude_index], [self.apogee])
                 self.apogee_text.setPos(altitude_index, self.apogee)
             elif nb_points == 1:
+                self.apogee_dict.clear()
+                self.apogee_dict.append(values[0])
+
                 self.apogee = values[0]
                 self.apogee_point.setData([], [])
                 self.apogee_text.setPos(0, self.apogee)
@@ -72,12 +76,35 @@ class AltitudeGraph(PlotWidget):
             self.current_altitude_point.setData([nb_points - 1], [self.current_altitude])
             self.current_altitude_text.setPos(nb_points - 1, self.current_altitude)
 
-            # self.setApogee(values) #Beaucoup plus lent.
+            # self.set_apogee(values) #Beaucoup plus lent.
+            # Fix: Faire une liste de tout les apogee possible, le mettre dans un dic, et remove si trop loin.
 
-            if self.current_altitude > self.apogee:
+            # if self.current_altitude > self.apogee:
+            #     self.apogee = self.current_altitude
+            #     self.apogee_point.setData([nb_points - 1], [self.current_altitude])
+            #     self.apogee_text.setPos(nb_points - 1, self.current_altitude)
+
+
+            #if len(self.apogee_dict) > 1 and self.apogee_dict.index(self.apogee_dict[-1]) < nb_points:
+
+            if self.current_altitude > self.apogee_dict[-1]:
+                self.apogee_dict.append(values[nb_points-1])
                 self.apogee = self.current_altitude
                 self.apogee_point.setData([nb_points - 1], [self.current_altitude])
                 self.apogee_text.setPos(nb_points - 1, self.current_altitude)
+
+            print(self.values.index(self.apogee_dict[-1]), nb_points)
+            rg_index = None
+            for i in range(len(values)):
+                if values[i] == self.apogee_dict[-1]:
+                    rg_index = i
+                    break
+            if rg_index > nb_points:
+                self.apogee_dict.pop()
+
+            # Il faut donc recuperer l index de la derniere apogee dans values.
+            # if len(self.apogee_dict) > 1 and self.values.index(self.apogee_dict[-1]) > nb_points:
+            #     self.apogee_dict.pop()
 
     def set_target_altitude(self, altitude):
         self.plotItem.addLine(y=altitude, pen=mkPen(color=(15, 236, 20), width=3, style=QtCore.Qt.DashDotLine))
