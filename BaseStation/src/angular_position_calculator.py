238715dc816e83a26e28ae0089d1ad91b2d1ecc2
Babouche <clnabergel@hotmail.fr>
2019-02-03 02:14:30 -0500

    Euler to quaternions
    + Formule correcte pour la conversion des angle d euler (radians) en quaternion.
    
    + Amelioration des test unitaires.

--- BaseStation/src/angular_position_calculator.py
+++ BaseStation/src/angular_position_calculator.py
@@ -1,17 +1,4 @@
-from math import *
-from numpy import *
-
-# Position en fonction de la vitesse. Integration numerique.
-# Penser a intergrer pour les anciennes annees.
-# Celon la vitesse angulaire, trouver les 3 angles en EULER
-
-# https://physics.stackexchange.com/questions/73961/angular-velocity-expressed-via-euler-angles
-# http://galileoandeinstein.physics.virginia.edu/7010/CM_26_Euler_Angles.html
-
-# Formule: https://computergraphics.stackexchange.com/questions/8195/how-to-convert-euler-angles-to-quaternions-and-get-the-same-euler-angles-back-fr
-# Spherical: https://www.omnicalculator.com/math/spherical-coordinates
-
-# Avec les coordonees 50, 10, 2, on doit obtenir en angle: 51.03, 87.75, 11.31
+from numpy import arccos, arctan, sqrt, radians, degrees, cos, sin
 
 
 class AngularCalculator:
@@ -19,33 +6,39 @@ class AngularCalculator:
     def __init__(self):
         self.angles = (0, 0, 0)
         self.quaternions = (0, 0, 0, 0)
-        pass
 
     def compute_angular_velocity(self, ang_velocity: tuple):
+        theta = 0
+        psi = 0
+
         r = sqrt(pow(ang_velocity[0], 2) + pow(ang_velocity[1], 2) + pow(ang_velocity[2], 2))  # x
-        theta = arccos(ang_velocity[2] / r)  # y
-        psi = arctan(ang_velocity[1] / ang_velocity[0])  # z
 
-        self.angles = r, theta, psi
-        self.euler_to_quaternion(degrees(psi), degrees(theta), r)  # XYZ
+        if r != 0:
+            theta = arccos(ang_velocity[2] / r)  # y
+        if ang_velocity[0] != 0:
+            psi = arctan(ang_velocity[1] / ang_velocity[0])  # z
 
-    def euler_to_quaternion(self, bank, heading, attitude):
-        c1 = cos(heading * 0.5)
-        c2 = cos(attitude * 0.5)
-        c3 = cos(bank * 0.5)
-        s1 = sin(heading * 0.5)
-        s2 = sin(attitude * 0.5)
-        s3 = sin(bank * 0.5)
+        self.angles = radians(r), theta, psi
+        self.euler_to_quaternion(self.angles[2], self.angles[1], self.angles[0])
 
-        qw = c1*c2*c3 + s1*s2*s3
-        qx = c1*c2*s3 - s1*s2*c3
-        qy = c1*s2*c3 + s1*c2*s3
-        qz = s1*c2*c3 - c1*s2*s3
+    # Z Y X | angles seulement en radians.
+    def euler_to_quaternion(self, yaw, pitch, roll):
+        cy = cos(yaw * 0.5)
+        sy = sin(yaw * 0.5)
+        cp = cos(pitch * 0.5)
+        sp = sin(pitch * 0.5)
+        cr = cos(roll * 0.5)
+        sr = sin(roll * 0.5)
+
+        qw = cy * cp * cr + sy * sp * sr
+        qx = cy * cp * sr - sy * sp * cr
+        qy = sy * cp * sr + cy * sp * cr
+        qz = sy * cp * cr - cy * sp * sr
 
         self.quaternions = qx, qy, qz, qw
 
     def get_angles_degrees(self):
-        return self.angles[0], degrees(self.angles[1]), degrees(self.angles[2])
+        return degrees(self.angles[0]), degrees(self.angles[1]), degrees(self.angles[2])
 
     def get_angles_radians(self):
         return self.angles
--- BaseStation/tests/test_angular_calculator.py
+++ BaseStation/tests/test_angular_calculator.py
@@ -17,20 +17,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(rounded_angle, (93.54, 74.5, 33.69))
 
     def test_euler_to_quaternion(self):
-        self.angular_calculator.euler_to_quaternion(90, 0, 0)
-        self.assertEqual(self.angular_calculator.get_quaternions(), (0.8509035245341184, 0.0, 0.0, 0.5253219888177297))
+        self.angular_calculator.euler_to_quaternion(0, 0, 3.14/2)
+        computed_quaternion = self.angular_calculator.get_quaternions()
+        rounded_quaternion = round(computed_quaternion[0], 3), round(computed_quaternion[1], 3), round(computed_quaternion[2], 3), round(computed_quaternion[3], 3)
+
+        self.assertEqual(rounded_quaternion, (0.707, 0.0, 0.0, 0.707))
 
     def test_full_class_euler_to_quaternion(self):
         angular_velocity = (75, 50, 25)
         self.angular_calculator.compute_angular_velocity(angular_velocity)
 
-        computed_angle = self.angular_calculator.get_angles_radians()
-        rounded_angle = round(computed_angle[0], 2), round(computed_angle[1], 2), round(computed_angle[2], 2)
-
-        self.angular_calculator.euler_to_quaternion(rounded_angle[0], rounded_angle[1], rounded_angle[2])
-
         computed_quaternion = self.angular_calculator.get_quaternions()
         rounded_quaternion = round(computed_quaternion[0], 3), round(computed_quaternion[1], 3), round(computed_quaternion[2], 3), round(computed_quaternion[3], 3)
 
-        self.assertEqual((0.675, 0.229, 0.580, 0.394), rounded_quaternion)
-
+        self.assertEqual((0.435, 0.565, -0.264, 0.65), rounded_quaternion)
