e14ae85f59876e728958fe75f737c60d6830f0a3
MaitreMenard <maxime.menard.1@ulaval.ca>
2019-02-07 14:36:50 -0500

    Ajout de setters pour tous les callbacks de MenuBar

--- BaseStation/src/ui/menu_bar.py
+++ BaseStation/src/ui/menu_bar.py
@@ -20,10 +20,10 @@ class MenuBar(QMenuBar):
         self.new_acquisition_action.setShortcut("Ctrl+N")
         self.new_acquisition_action.setStatusTip("Démarre une nouvelle acquisition des données de vol")
 
-        self.open_csv_file_action = QAction("Ouvrir un fichier CSV", files_menu)
-        self.open_csv_file_action.setObjectName("open_csv_file_action")
-        self.open_csv_file_action.setShortcut("Ctrl+O")
-        self.open_csv_file_action.setStatusTip("Ouvre le mode replay avec les données de vol sélectionnées")
+        self.load_flight_data_action = QAction("Charger des données de vol", files_menu)
+        self.load_flight_data_action.setObjectName("load_flight_data_action")
+        self.load_flight_data_action.setShortcut("Ctrl+O")
+        self.load_flight_data_action.setStatusTip("Ouvre le mode replay avec les données de vol sélectionnées")
 
         self.open_simulation_action = QAction('&Ajouter une simulation', files_menu)
         self.open_simulation_action.setShortcut('Ctrl+A')
@@ -34,15 +34,21 @@ class MenuBar(QMenuBar):
         self.exit_action.setStatusTip("Quitte l'application")
 
         files_menu.addAction(self.new_acquisition_action)
-        files_menu.addAction(self.open_csv_file_action)
+        files_menu.addAction(self.load_flight_data_action)
         files_menu.addAction(self.open_simulation_action)
         files_menu.addSeparator()
         files_menu.addAction(self.exit_action)
 
         self.addAction(files_menu.menuAction())
 
-    def set_open_simulation_callback(self, callback: Callable):
+    def set_new_acquisition_callback(self, callback: Callable[[], None]):
+        self.new_acquisition_action.triggered.connect(callback)
+
+    def set_load_flight_data_callback(self, callback: Callable[[], None]):
+        self.load_flight_data_action.triggered.connect(callback)
+
+    def set_open_simulation_callback(self, callback: Callable[[], None]):
         self.open_simulation_action.triggered.connect(callback)
 
-    def set_on_exit_callback(self, callback: Callable):
+    def set_on_exit_callback(self, callback: Callable[[], None]):
         self.exit_action.triggered.connect(callback)
