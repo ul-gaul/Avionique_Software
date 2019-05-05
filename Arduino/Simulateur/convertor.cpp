83322ee7490f51867cf1d177bece8e0b598c2a85
MaitreMenard <maxime.menard.1@ulaval.ca>
2019-05-05 14:01:39 -0400

    Fix bug nombre décimaux [0, 1[ dans le simulateur Arduino

--- Arduino/Simulateur/convertor.cpp
+++ Arduino/Simulateur/convertor.cpp
@@ -4,7 +4,13 @@ void toDouble(float value, byte outputDouble[8]) {
     byte* floatBytes = (byte*)&value;
 
     byte floatExponent = ((floatBytes[3] & BITS_6_0_MASK) << 1) | ((floatBytes[2] & BIT_7_MASK) >> 7);
-    uint16_t doubleExponent = (uint16_t)(floatExponent - FLOAT_EXPONENT_OFFSET) + DOUBLE_EXPONENT_OFFSET;   //FIXME: handle negative exponent correctly
+
+    uint16_t doubleExponent;
+    if (floatExponent == 0x00) {
+        doubleExponent = 0x00;
+    } else {
+        doubleExponent = (int16_t)(floatExponent - FLOAT_EXPONENT_OFFSET) + DOUBLE_EXPONENT_OFFSET;
+    }
     byte* doubleExponentBytes = (byte*)&doubleExponent;
     
     outputDouble[7] = (floatBytes[3] & BIT_7_MASK) | ((doubleExponentBytes[1] & BITS_2_0_MASK) << 4) | ((doubleExponentBytes[0] & BITS_7_4_MASK) >> 4);
