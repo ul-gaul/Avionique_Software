e14e7f2e8db116eaad42967f59a384c79b6a70a9
Chiminakust <maxime_glmt@hotmail.ca>
2020-03-28 12:50:41 -0400

    added CRC computation before sending the message

--- cli/main.py
+++ cli/main.py
@@ -5,12 +5,16 @@
 import sys
 import serial
 import struct
+import pycrc.algorithms as crcalgo
 
 
 # global configuration variables
-BAUD_RFD = 9600
-CMD_PKT_FMT = '>HHBBH'
+BAUD_RFD = 115200
+CMD_PKT_FMT = '>HHBB'
 START_SHORT = 0xface
+CRC = crcalgo.Crc(width=16, poly=0x1021,
+				reflect_in=False, reflect_out=False,
+				xor_in=0xffff, xor_out=0x0000)
 
 
 def print_help():
@@ -20,33 +24,56 @@ def print_help():
 	print('\targ is the number of the actuator')
 	print('reset - reset an actuator to a low state')
 	print('\targ is the number of the actuator')
+	print('exit - quit the command line tool')
+
+
+def compute_crc(data):
+	return CRC.bit_by_bit(data)
 
 
 def set_actuator(ser, i, a):
-	# TODO: use a CRC
-	data = struct.pack(CMD_PKT_FMT, START_SHORT, i, 1, a, 0)
-	ser.write(data)
+	data = struct.pack(CMD_PKT_FMT, START_SHORT, i, 1, a)
+	data = struct.pack(CMD_PKT_FMT + 'H', data, compute_crc(data))
+	print('writing:', data)
+	size = ser.write(data)
+	print('wrote:', size)
 
 
 def reset_actuator(ser, i, a):
-	# TODO: use a CRC
-	data = struct.pack(CMD_PKT_FMT, START_SHORT, i, 2, a, 0)
-	ser.write(data)
+	data = struct.pack(CMD_PKT_FMT, START_SHORT, i, 2, a)
+	data = struct.pack(CMD_PKT_FMT + 'H', data, compute_crc(data))
+	print('writing:', data)
+	size = ser.write(data)
+	print('wrote:', size)
 
 
 def cli(port):
 	i = 0
 	with serial.Serial(port, BAUD_RFD, timeout=1) as ser:
 		while True:
-			f, a = input('> ').split(' ')
-			if f == '-h' or f == '--help':
+			print(ser.timeout)
+			# get input
+			s = input('> ')
+			# treat simple functions first
+			if s == '-h' or s == '--help':
 				print_help()
-			elif f == 'set':
-				set_actuator(ser, i, int(a))
+			elif s == 'exit':
+				break
+			# get function and arguments
+			try:
+				f, a = s.split(' ')
+				a = int(a) 
+			except ValueError as e:
+				print('Invalid command, error occured:', e)
+				continue
+			# treat function calls
+			if f == 'set':
+				set_actuator(ser, i, a)
 			elif f == 'reset':
-				reset_actuator(ser, i, int(a))
+				reset_actuator(ser, i, a)
 			else:
 				print('Invalid command, type -h or --help for help')
+			i += 1
 
 
 if __name__ == '__main__':
